05cc7c00a8a1ca5afd06b27b367c6fdef99cb76d
Kondek <588475@CTS.COM>
2016-10-12 16:22:50 -0300

    No me acuerdo que hice

--- WebContent/js/controllers/recibos.controller.js
+++ WebContent/js/controllers/recibos.controller.js
@@ -5,4 +5,5 @@ angular
         $scope.firmadoSI = true;
     
         $scope.months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+        $scope.years = ["2016", "2015", "2014", "2013", "2012", "2011", "2010", "2009", "2008", "2007", "2006"];
     });
