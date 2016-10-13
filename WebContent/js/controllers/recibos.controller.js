03ef5f855d065f0edfd28d1b26e8ec03493745c8
Kondek <588475@CTS.COM>
2016-10-13 14:34:52 -0300

    Resuelto varios error y agregado solapa usuarios

--- WebContent/js/controllers/recibos.controller.js
+++ WebContent/js/controllers/recibos.controller.js
@@ -3,7 +3,9 @@ angular
     .controller("recibosCtrl", function ($scope) {
         $scope.firmadoNO = false;
         $scope.firmadoSI = true;
-    
+
         $scope.months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
         $scope.years = ["2016", "2015", "2014", "2013", "2012", "2011", "2010", "2009", "2008", "2007", "2006"];
+
+        $scope.problemas = ["Problema 1", "Problema 2", "Problema 3", "Problema 4", "Problema 5", "Otro"];
     });
